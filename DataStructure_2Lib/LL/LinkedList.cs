7d1d3bc41367b1fa229777b98062d424ad4d3864
Dmitriy <banodannon@gmail.com>
2020-11-12 21:08:07 +0300

    1-15 except reverse

---
+++ DataStructure_2Lib/LL/LinkedList.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure_2Lib.LL
+{
+    public class LinkedList
+    {
+        public int Length { get; set; }
+
+        private Node _root;
+
+        //private Node _last;
+
+        public int this[int index]
+        {
+            get
+            {
+                if ((index>=Length)||(index<0))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                Node tmp = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                return tmp.Value;
+            }
+
+            set //должен ли быть private?
+            {
+                if ((index >= Length) || (index < 0))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                Node tmp = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                tmp.Value = value;
+            }
+        }
+
+        public LinkedList()
+        {
+            Length = 0;
+            _root = null;
+        }
+
+        public LinkedList(int value)
+        {
+            _root = new Node(value);
+            Length = 1;
+        }
+
+        public LinkedList(int[] array)
+        {
+            if (array.Length != 0)
+            {
+                _root = new Node(array[0]);
+                Node tmp = _root;
+
+                for (int i = 1; i < array.Length; i++)
+                {
+                    tmp.Next = new Node(array[i]);
+                    tmp = tmp.Next;
+                }
+
+                Length = array.Length;
+            }
+            else
+            {
+                Length = 0;
+                _root = null;
+            }
+        }
+
+        public void AddByIndex(int index, int value)
+        {
+            if ((index>Length)||(index<0))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (index == 0)
+            {
+                Node tmp = _root;
+                _root = new Node(value);
+                _root.Next = tmp;
+            }
+            else
+            {
+                Node current = _root;
+                for (int i = 1; i < index; i++)
+                {
+                    current = current.Next;
+                }
+
+                Node tmp = current.Next;
+                current.Next = new Node(value);
+
+                current.Next.Next = tmp;
+            }
+            Length++;
+        }
+
+        public void DeleteLast()
+        {
+            if (Length == 0)
+            {
+                return;
+            }
+
+            Node current = _root;
+            for (int i = 1; i < Length - 1; i++)
+            {
+                current = current.Next;
+            }
+
+            // current.Next?.Dispose();// если Next == null
+
+            current.Next = null;
+            Length--;
+            
+        }
+
+        public void DeleteFirst()
+        {
+            if (Length == 0)
+            {
+                return;
+            }
+
+            Node current = _root.Next;//запись в current ссылки на на второй элемент
+            _root = current;//замена ссылки на первый элемент ссылкой на второй элемент
+
+            // current.Next?.Dispose();// если Next == null
+
+            Length--;
+        }
+
+        public void DeleteByIndex(int index)
+        {
+            if (Length == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if ((index > Length-1) || (index < 0))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (index == 0)
+            {
+                Node tmp = new Node();
+                tmp = _root.Next;
+                _root = tmp;
+            }
+            else
+            {
+                Node current = _root;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+
+                // current.Next?.Dispose();// если Next == null
+
+
+                current.Next = current.Next.Next;
+            }
+            Length--;
+        }
+
+        public int GetLength()
+        {
+            return Length;
+        }
+
+        public int GetIndexByValue(int value)
+        {
+            int index = 0;
+            Node current = new Node();
+            current = _root;
+
+            while(current.Value != value)
+            {
+                index++;
+                current = current.Next;
+                if (current.Next == null) throw new NullReferenceException();
+            }
+
+            return index;
+        }
+
+        public void Reverse()
+        {
+            
+        }
+        
+        public int GetMaxElement()
+        {
+            Node current = new Node();
+            current = _root;
+            int max = current.Value;
+
+            if (Length==0)
+            {
+                throw new NullReferenceException();
+            }
+
+            do
+            {
+                if ((current.Next!=null)&&(max < current.Next.Value))
+                {
+                    max = current.Next.Value;
+                }
+                if (current.Next != null)
+                {
+                    current = current.Next;
+                }
+            }
+            while (current.Next != null);
+            
+            return max;
+        }
+
+        public int GetMinElement()
+        {
+            Node current = new Node();
+            current = _root;
+            int min = current.Value;
+
+            if (Length == 0)
+            {
+                throw new NullReferenceException();
+            }
+
+            do
+            {
+                if ((current.Next != null) && (min > current.Next.Value))
+                {
+                    min = current.Next.Value;
+                }
+                if (current.Next != null)
+                {
+                    current = current.Next;
+                }
+            }
+            while (current.Next != null);
+
+            return min;
+        }
+
+        public int GetIndexOfMaxElement()
+        {
+            Node current = new Node();
+            current = _root;
+            int max = current.Value;
+            int index = 0;
+            int indexMax = index;
+
+            if (Length == 0)
+            {
+                throw new NullReferenceException();
+            }
+
+            do
+            {
+                
+                if ((current.Next != null) && (max < current.Next.Value))
+                {
+                    max = current.Next.Value;
+                    indexMax = index+1;
+                    
+                }
+                if (current.Next != null)
+                {
+                    current = current.Next;
+                    index++;
+                }
+            }
+            while (current.Next != null);
+
+            return indexMax;
+        }
+
+        public int GetIndexOfMinElement()
+        {
+            Node current = new Node();
+            current = _root;
+            int min = current.Value;
+            int index = 0;
+            int indexMin = index;
+
+            if (Length == 0)
+            {
+                throw new NullReferenceException();
+            }
+
+            do
+            {
+                if ((current.Next != null) && (min > current.Next.Value))
+                {
+                    min = current.Next.Value;
+                    indexMin = index + 1;
+
+                }
+                if (current.Next != null)
+                {
+                    current = current.Next;
+                    index++;
+                }
+            }
+            while (current.Next != null);
+
+            return indexMin;
+        }
+
+        public override bool Equals(object obj)
+        {
+            LinkedList linkedList = (LinkedList)obj;
+
+            if (Length != linkedList.Length)
+            {
+                return false;
+            }
+
+            //for (int i = 0; i < Length; i++)
+            //{
+            //    if(this[i]!=linkedList[i])
+            //    {
+            //        return false;
+            //    }
+            //}
+
+            Node tmp1 = _root;
+            Node tmp2 = linkedList._root;
+            for (int i = 0; i < Length; i++)
+            {
+                if (tmp1.Value != tmp2.Value)
+                {
+                    return false;
+                }
+                tmp1 = tmp1.Next;
+                tmp2 = tmp2.Next;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+
+            if (_root != null)
+            {
+                Node tmp = _root;
+
+                while (tmp != null)
+                {
+                    s += tmp.Value + ";";
+                    tmp = tmp.Next;
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ LLTests/LinkedListTests.cs
@@ -0,0 +1,235 @@
+using NUnit.Framework;
+using System;
+using DataStructure_2Lib.LL;
+//using DataStructure.LL;
+
+namespace LLTests
+{
+    public class Tests
+    {
+        public class LinkedListTests
+        {
+            [TestCase(new int[] { 1, 2, 3 }, 1, 4, new int[] { 1, 4, 2, 3 })]
+            [TestCase(new int[] { 1, 2, 3 }, 0, 4, new int[] { 4, 1, 2, 3 })]
+            [TestCase(new int[] { 1, 2, 3 }, 3, 4, new int[] { 1, 2, 3, 4 })]
+            [TestCase(new int[] { }, 0, 4, new int[] { 4 })]
+            [TestCase(new int[] { 1 }, 0, 4, new int[] { 4, 1 })]
+            public void AddByIndexTest(int[] array, int index, int value, int[] expArray)
+            {
+                LinkedList expected = new LinkedList(expArray);
+                LinkedList actual = new LinkedList(array);
+
+                actual.AddByIndex(index, value);
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { 1, 2, 3 }, 5, 4)]
+            public void AddByIndexTestNegative(int[] array, int index, int value)
+            {
+                LinkedList a = new LinkedList(array);
+
+
+                Assert.Throws<IndexOutOfRangeException>(() => a.AddByIndex(index, value));
+            }
+
+            [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 2 })]
+            [TestCase(new int[] { 1, 2 }, new int[] { 1 })]
+            [TestCase(new int[] { 1 }, new int[] { })]
+            [TestCase(new int[] { }, new int[] { })]
+            public void DeleteLastTest(int[] a, int[] b)
+            {
+                LinkedList expected = new LinkedList(b);
+                LinkedList actual = new LinkedList(a);
+                actual.DeleteLast();
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { 1, 2, 3 }, new int[] { 2, 3 })]
+            public void DeleteFirstTest(int[] a, int[] b)
+            {
+                LinkedList expected = new LinkedList(b);
+                LinkedList actual = new LinkedList(a);
+                actual.DeleteFirst();
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { 1, 2, 3, 4 }, 0, new int[] { 2, 3, 4 })]
+            [TestCase(new int[] { 1, 2, 3, 4 }, 1, new int[] { 1, 3, 4 })]
+            [TestCase(new int[] { 1, 2, 3, 4 }, 3, new int[] { 1, 2, 3 })]
+            [TestCase(new int[] { 1 }, 0, new int[] { })]
+            public void DeleteByIndexTest(int[] actualList, int index, int[] expectedList)
+            {
+                LinkedList expected = new LinkedList(expectedList);
+                LinkedList actual = new LinkedList(actualList);
+                actual.DeleteByIndex(index);
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { }, 0)]
+            [TestCase(new int[] { }, -1)]
+            [TestCase(new int[] { 3 }, -1)]
+            [TestCase(new int[] { 1, 2 }, 2)]
+            [TestCase(new int[] { 1, 2 }, -1)]
+            public void DeleteByIndexTestNegative(int[] actualList, int index)
+            {
+                LinkedList a = new LinkedList(actualList);
+
+
+                Assert.Throws<IndexOutOfRangeException>(() => a.DeleteByIndex(index));
+            }
+
+            [TestCase(new int[] { 1, 2, 3, 4 }, 4)] //нужен ли негативный тест?
+            [TestCase(new int[] { }, 0)]
+            public void GetLengthTest(int[] array, int expected)
+            {
+                LinkedList a = new LinkedList(array);
+                int actual = a.GetLength();
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { 1, 2, 3, 4 }, 2, 3)]
+            public void GetByIndexTest(int[] array, int index, int expected)
+            {
+                LinkedList a = new LinkedList(array);
+                int actual = a[index];
+
+                a[index] = 321;
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { 1, 2, 3, 4 }, 4)]
+            [TestCase(new int[] { 1, 2, 3, 4 }, -1)]
+            public void GetByIndexTestNegative(int[] array, int index)
+            {
+                LinkedList a = new LinkedList(array);
+
+
+                Assert.Throws<IndexOutOfRangeException>(() => a[index]++);
+            }
+
+            [TestCase(new int[] { 0, 1, 1, 1, 1 }, 1, 1)]
+            [TestCase(new int[] { 1 }, 1, 0)]
+            public void GetIndexByValueTest(int[] array, int value, int expected)
+            {
+                LinkedList a = new LinkedList(array);
+                int actual = a.GetIndexByValue(value);
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { }, 1)]
+            [TestCase(new int[] { 2 }, 1)]
+            public void GetIndexByValueTestNegative(int[] array, int value)
+            {
+                LinkedList a = new LinkedList(array);
+                int actual;
+
+                Assert.Throws<NullReferenceException>(() => actual = a.GetIndexByValue(value));
+            }
+
+            [TestCase(new int[] { 1, 2, 3, 4 }, 3, 5, new int[] { 1, 2, 3, 5 })]
+            public void SetByIndexTest(int[] actualArray, int index, int value, int[] expectedArray)
+            {
+                LinkedList actual = new LinkedList(actualArray);
+                LinkedList expected = new LinkedList(expectedArray);
+
+                actual[index] = value;
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestCase(new int[] { 1, 2, 3, 4 }, 4, 5)]
+            [TestCase(new int[] { 1, 2, 3, 4 }, -4, 5)]
+            [TestCase(new int[] { }, 4, 5)]
+            [TestCase(new int[] { }, -4, 5)]
+            public void SetByIndexTestNegative(int[] actualArray, int index, int value)
+            {
+                LinkedList actual = new LinkedList(actualArray);
+
+                Assert.Throws<IndexOutOfRangeException>(() => actual[index] = value);
+            }
+
+            [Test]
+            public void ReverseTest()
+            {
+
+            }//НЕ СДЕЛАНО
+
+            [TestCase(new int[] { 1, 4, 8, 5 }, 8)]
+            [TestCase(new int[] { 1, -4, 8, 5 }, 8)]
+            [TestCase(new int[] { -1, -4, -8, -5 }, -1)]
+            [TestCase(new int[] { -4, -4, -8, -5 }, -4)]
+            [TestCase(new int[] { -4 }, -4)]
+            [TestCase(new int[] { 0 }, 0)]
+            [TestCase(new int[] { 0, 0 }, 0)]
+            public void GetMaxElementTest(int[] arrayForActual, int expected)
+            {
+                LinkedList actual = new LinkedList(arrayForActual);
+
+                Assert.AreEqual(expected, actual.GetMaxElement());
+            }
+
+            [TestCase(new int[] { })]
+            public void GetMaxElementTestNegative(int[] arrayForActual)
+            {
+                LinkedList actual = new LinkedList(arrayForActual);
+
+                Assert.Throws<NullReferenceException>(() => actual.GetMaxElement());
+            }
+
+            [TestCase(new int[] { 1, 4, 8, 5 }, 1)]
+            [TestCase(new int[] { 1, -4, 8, 5 }, -4)]
+            [TestCase(new int[] { -1, -4, -8, -5 }, -8)]
+            [TestCase(new int[] { -4 }, -4)]
+            [TestCase(new int[] { 0 }, 0)]
+            [TestCase(new int[] { 1, 0, -1, 1 }, -1)]
+            public void GetMinElementTest(int[] arrayForActual, int expected)
+            {
+                LinkedList actual = new LinkedList(arrayForActual);
+
+
+                Assert.AreEqual(expected, actual.GetMinElement());
+            }
+
+            [TestCase(new int[] { })]
+            public void GetMinElementTestNegative(int[] arrayForActual)
+            {
+                LinkedList actual = new LinkedList(arrayForActual);
+
+                Assert.Throws<NullReferenceException>(() => actual.GetMinElement());
+            }
+
+            [TestCase(new int[] { -4 }, 0)]
+            [TestCase(new int[] { 0 }, 0)]
+            [TestCase(new int[] { 1, 0, 2, -1 }, 2)]
+            [TestCase(new int[] { 0, 0, -1, 1 }, 3)]
+            [TestCase(new int[] { 0, 0, -1, 3 }, 3)]
+            public void GetIndexOfMaxElementTest(int[] arrayForActual, int expected)
+            {
+                LinkedList actual = new LinkedList(arrayForActual);
+
+
+                Assert.AreEqual(expected, actual.GetIndexOfMaxElement());
+            } 
+            
+            [TestCase(new int[] { -4 }, 0)]
+            [TestCase(new int[] { 0 }, 0)]
+            [TestCase(new int[] { 1, 0, -1, 3 }, 2)]
+            [TestCase(new int[] { 0, -20, -1, 3 }, 1)]
+            public void GetIndexOfMinElementTest(int[] arrayForActual, int expected)
+            {
+                LinkedList actual = new LinkedList(arrayForActual);
+
+
+                Assert.AreEqual(expected, actual.GetIndexOfMinElement());
+            }
+
+        }
+    }
+}
